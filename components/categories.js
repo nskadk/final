1488af0ec49fcb507e02f1790eb1c6295060bc3f
nskadk <118862276+nskadk@users.noreply.github.com>
2023-10-14 19:33:16 +0700

    first commit

--- components/categories.js
+++ components/categories.js
@@ -1,4 +1,4 @@
-import { View, Text, FlatList, Image, TouchableOpacity} from 'react-native'
+import { View, Text, SafeAreaView, Image, TouchableOpacity} from 'react-native'
 import React from 'react'
 import { categories } from '../constants'
 
@@ -7,20 +7,17 @@ import { categories } from '../constants'
 export default function Categories() {
 
   return (
-    <View style={{ marginHorizontal: 0 }}>
-      <FlatList
-        data={categories}
-        numColumns={3} // Số cột
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => (
-          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-between', paddingTop: 20 }}>
+    <SafeAreaView style={{ marginHorizontal: 0 }}>
+      <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+        {categories.map((item, index) => (
+          <View key={index} style={{ width: '33%', alignItems: 'center', justifyContent: 'space-between', paddingTop: 20 }}>
             <TouchableOpacity>
               <Image style={{ width: 70, height: 70 }} source={item.image} />
             </TouchableOpacity>
-            <Text style={{paddingTop: 10}}>{item.name}</Text>
+            <Text style={{ paddingTop: 10 }}>{item.name}</Text>
           </View>
-        )}
-      />
-    </View>
+        ))}
+      </View>
+    </SafeAreaView>
   )
 }
