2deeab49b6a8b5f83950a00cd925fca2d138eb5e
Ha Phan Hai <ha.phanhai4869@hcmut.edu.vn>
2023-10-15 08:21:57 +0700

    fix map

--- screens/Map.js
+++ screens/Map.js
@@ -1,8 +1,10 @@
 import MapView, { LatLng, Marker, PROVIDER_GOOGLE } from "react-native-maps"
-import { StyleSheet, View, Dimensions, Text, TouchableOpacity } from "react-native"
+import { StyleSheet, View, Dimensions, Text, TouchableOpacity, Image, SafeAreaView } from "react-native"
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete"
 import { GOOGLE_API_KEY } from "@env"
 import Constants from "expo-constants"
+import { location } from "../data/location"
+import { useNavigation } from "@react-navigation/native"
 
 // https://docs.expo.dev/versions/latest/sdk/map-view/
 // https://www.npmjs.com/package/react-native-google-places-autocomplete
@@ -14,35 +16,38 @@ const ASPECT_RATIO = width / height
 const LATITUDE_DELTA = 0.02
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
 const INITIAL_POSITION = {
-	latitude: 10.7808,
-	longitude: 106.7569,
+	latitude: 10.76487736929764,
+	longitude: 106.6555937375142,
 	latitudeDelta: LATITUDE_DELTA,
 	longitudeDelta: LONGITUDE_DELTA,
 }
 
-// type InputAutocompleteProps = {
-//   label: string;
-//   placeholder?: string;
-//   onPlaceSelected: (details: GooglePlaceDetail | null) => void;
-// };
-
 export default function App() {
 	return (
-		<View style={styles.container}>
+		<SafeAreaView style={styles.container}>
 			<MapView
 				style={styles.map}
 				provider={PROVIDER_GOOGLE}
 				initialRegion={INITIAL_POSITION}>
 				<Marker coordinate={INITIAL_POSITION} />
+				{location.map((item) => (
+					<Marker
+						key={item.id}
+						coordinate={{
+							latitude: item.latitude,
+							longitude: item.longitude,
+						}}
+						title={item.name}
+						description={item.address}>
+						<Image
+							source={require("../assets/images/manufacturer.png")}
+							style={{ height: 35, width: 35 }}
+						/>
+					</Marker>
+				))}
 			</MapView>
-			<GooglePlacesAutocomplete
-				placeholder="search"
-				onPress={(data, details = null) => {
-					console.log(data, details)
-				}}
-				query={{ key: { GOOGLE_API_KEY }, language: "vi" }}
-			/>
-		</View>
+			<GooglePlacesAutocomplete query={{ key: { GOOGLE_API_KEY }, language: "vi" }} />
+		</SafeAreaView>
 	)
 }
 
@@ -56,31 +61,5 @@ const styles = StyleSheet.create({
 	map: {
 		width: Dimensions.get("window").width,
 		height: Dimensions.get("window").height,
-	},
-	searchContainer: {
-		position: "absolute",
-		width: "90%",
-		backgroundColor: "white",
-		shadowColor: "black",
-		shadowOffset: { width: 2, height: 2 },
-		shadowOpacity: 0.5,
-		shadowRadius: 4,
-		elevation: 4,
-		padding: 8,
-		borderRadius: 8,
-		top: Constants.statusBarHeight,
-	},
-	input: {
-		borderColor: "#888",
-		borderWidth: 1,
-	},
-	button: {
-		backgroundColor: "#bbb",
-		paddingVertical: 12,
-		marginTop: 16,
-		borderRadius: 4,
-	},
-	buttonText: {
-		textAlign: "center",
 	},
 })
